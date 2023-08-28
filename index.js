620a542291b1454e3d1f625067fabe02d15ca77e
misafxd <pisichu.mafs@gmail.com>
2023-08-28 00:26:47 -0600

    Improve draw functions

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const color = document.querySelector('#pixel-color');
 const reset = document.querySelector('.reset');
 let choice = 16;
 const CONTAINER_SIZE = 30;
+let clickTimeout;
+let clickedElement;
 
 function clearGrid(){
     if (container.firstChild){
@@ -15,43 +17,46 @@ function clearGrid(){
         }
 }
 
-function drawGrid(gridChoice = 16){
-    let pixelWidth = (CONTAINER_SIZE/gridChoice) + "rem";
-    for(let i = 0; i < (gridChoice * gridChoice); i++){
+function drawGrid(gridChoice = 16) {
+    let pixelWidth = (CONTAINER_SIZE / gridChoice) + "rem";
+    let isMousePressed = false;
+
+    function changeColor(e) {
+        if (isMousePressed && e.target.tagName === 'DIV' && !e.target.classList.contains('clicked')) {
+            e.target.style.backgroundColor = color.value;
+            e.target.classList.add('active');
+        }
+    }
+
+    container.addEventListener('mousedown', () => {
+        isMousePressed = true;
+    });
+
+    container.addEventListener('mouseup', () => {
+        isMousePressed = false;
+    });
+
+    container.addEventListener('mousemove', changeColor);
+
+    for (let i = 0; i < (gridChoice * gridChoice); i++) {
         const pixel = document.createElement('div');
         pixel.style.width = pixelWidth;
         pixel.style.height = pixelWidth;
         pixel.classList.add('pixel');
 
-        pixel.addEventListener('mouseover', (e) => {
-            if(e.target.tagName ==='DIV' && !e.target.classList.contains('clicked')){
+        pixel.addEventListener('click', (e) => {
+            e.target.classList.toggle('clicked');
+            if (e.target.classList.contains('clicked')) {
                 e.target.style.backgroundColor = color.value;
                 e.target.classList.add('active');
-            }
-        } );
-
-        pixel.addEventListener('mouseout', (e) => {
-            if(e.target.tagName === 'DIV' && !e.target.classList.contains('clicked')){
+            } else {
                 e.target.style.backgroundColor = 'initial';
                 e.target.classList.remove('active');
             }
-        })
-
-        pixel.addEventListener('click',(e) =>{
-            if(e.target.tagName === 'DIV'){
-                e.target.classList.toggle('clicked');
-                if (e.target.classList.contains('clicked')) {
-                    e.target.style.backgroundColor = color.value;
-                    e.target.classList.add('active');
-                } else {
-                    e.target.style.backgroundColor = 'initial';
-                    e.target.classList.remove('active');
-                }
-            }
         });
- 
+
         container.appendChild(pixel);
-}
+    }
 }
 
 gridSize.addEventListener('click', () => {
