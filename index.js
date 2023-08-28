4651e4da9870b6b3cd349a459802c3819a2adaff
misafxd <pisichu.mafs@gmail.com>
2023-08-27 18:17:34 -0600

    Add grid size button with its fuctions

--- index.js
+++ index.js
@@ -1,13 +1,33 @@
 const container = document.querySelector('.container');
+const gridSize = document.querySelector('.grid-size');
 const CONTAINER_SIZE = 30;
-let gridChoice = 16;
 
-let pixelWidth = (CONTAINER_SIZE/gridChoice) + "rem";
+function clearGrid(){
+    if (container.firstChild){
+        while (container.firstChild){
+            container.removeChild(container.firstChild);
+        }
+    } else {
+         return
+        }
+}
 
-for(let i = 0; i < (gridChoice * gridChoice); i++){
+function drawGrid(gridChoice){
+
+    let pixelWidth = (CONTAINER_SIZE/gridChoice) + "rem";
+    for(let i = 0; i < (gridChoice * gridChoice); i++){
         const pixel = document.createElement('div');
         pixel.style.width = pixelWidth;
         pixel.style.height = pixelWidth;
         pixel.classList.add('pixel');
         container.appendChild(pixel);
 }
+}
+
+gridSize.addEventListener('click', () => {
+    let gridChoice = prompt('Grid size: ');
+    clearGrid();
+    drawGrid(gridChoice);
+})
+
+
